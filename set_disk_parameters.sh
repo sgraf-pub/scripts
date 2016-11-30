44d4a394de75d56f6fea813adabcea87158c6362
Stanislav Graf <stanislav.graf@gmail.com>
2016-11-30 21:41:22 +0100

    30min -> 60min standby

--- set_disk_parameters.sh
+++ set_disk_parameters.sh
@@ -1,5 +1,10 @@
 #/bin/bash -x
 
 for disk in /dev/sd{a..g}; do
-    smartctl --set aam,off --set apm,128 --set lookahead,on --set wcache,on --set wcreorder,on --set standby,241 $disk
+    smartctl --set aam,off --set apm,128 --set lookahead,on --set wcache,on --set wcreorder,on --set standby,242 $disk
 done
+
+for disk in /dev/sd{b..c}; do
+    sdparm --set STANDBY=1,SCT=36000 --save $disk
+done
+

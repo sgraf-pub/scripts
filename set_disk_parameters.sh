279ab9dbaa3cc42ca79810384ea94ec4518e522c
Stanislav Graf <stanislav.graf@gmail.com>
2017-12-03 15:56:50 +0100

    Disable write cache

--- set_disk_parameters.sh
+++ set_disk_parameters.sh
@@ -1,10 +1,6 @@
 #/bin/bash -x
 
 for disk in /dev/sd{b..g}; do
-    smartctl --set aam,off --set apm,128 --set lookahead,on --set wcache,on --set wcreorder,on --set standby,242 $disk
-done
-
-for disk in /dev/sd{b..g}; do
-    sdparm --set STANDBY=1,SCT=36000 --save $disk
+    smartctl --set aam,off --set apm,128 --set lookahead,on --set wcache,off --set wcreorder,off --set standby,241 $disk
 done
 

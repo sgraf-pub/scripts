eff8d7dec3abf9ece8f89d32c840206dfd049e78
Stanislav Graf <stanislav.graf@gmail.com>
2018-02-02 21:33:09 +0100

    Make disk params simpler

--- set_disk_parameters.sh
+++ set_disk_parameters.sh
@@ -1,6 +1,6 @@
 #/bin/bash -x
 
-for disk in /dev/sd{b..g}; do
+for disk in /dev/sd?; do
     smartctl --set aam,off --set apm,128 --set lookahead,on --set wcache,off --set wcreorder,off --set standby,241 $disk
 done
 

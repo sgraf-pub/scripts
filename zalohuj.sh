00a674b685ce31aad80df69e716a1a6011aa5fc7
Stanislav Graf <stanislav.graf@gmail.com>
2018-07-15 12:34:16 +0200

    Backup also deduplicates

--- zalohuj.sh
+++ zalohuj.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+# Deduplication DATA
+/usr/sbin/duperemove -hdr --hashfile=/var/tmp/duperemove_data.hash /media/DATA/
+
 # Balance DATA
 btrfs balance start -musage=0 -v /media/DATA/
 btrfs balance start -musage=20 -v /media/DATA/
@@ -9,20 +12,26 @@ btrfs balance start -dusage=20 -v /media/DATA/
 # Mount BACKUP RW
 mount -o remount,rw /media/BACKUP
 
+# Sync DATA with BACKUP
+#cp      --archive --no-clobber --verbose \
+#        --one-file-system /media/DATA/* /media/BACKUP/
+
+rsync   --archive --delete --verbose \
+        --one-file-system /media/DATA/ /media/BACKUP/
+
+# Deduplication of BACKUP
+/usr/sbin/duperemove -hdr --hashfile=/var/tmp/duperemove_backup.hash /media/BACKUP/
+
+# Backup index files
+cp --force /var/tmp/duperemove* /media/DATA/Zalohy/backup/
+
 # Balance BACKUP
 btrfs balance start -musage=0 -v /media/BACKUP/
 btrfs balance start -musage=20 -v /media/BACKUP/
 btrfs balance start -dusage=0 -v /media/BACKUP/
 btrfs balance start -dusage=20 -v /media/BACKUP/
 
-# Sync DATA with BACKUP
-cp      --archive --no-clobber --verbose \
-        --one-file-system /media/DATA/* /media/BACKUP/
-
-rsync   --archive --delete --hard-links --verbose \
-        --one-file-system /media/DATA/ /media/BACKUP/
-
 # Let the things calm down and move BACKUP back to RO
-sleep 1m
+sleep 2m
 mount -o remount,ro /media/BACKUP
 

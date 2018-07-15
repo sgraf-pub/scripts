08a753d9eee5de633824e33a26c7515e66e08b56
Stanislav Graf <stanislav.graf@gmail.com>
2018-07-15 12:34:54 +0200

    Only when disk is not in sleep

--- set_disk_parameters.sh
+++ set_disk_parameters.sh
@@ -1,6 +1,19 @@
 #/bin/bash -x
 
 for disk in /dev/sd?; do
-    smartctl --set aam,off --set apm,128 --set lookahead,on --set wcache,off --set wcreorder,off --set standby,241 $disk
+    if  smartctl --quietmode errorsonly \
+                 --info \
+                 --nocheck standby \
+                 $disk &>/dev/null ; then
+
+        echo $disk
+        smartctl --set aam,off \
+                 --set apm,128 \
+                 --set lookahead,on \
+                 --set wcache,off \
+                 --set wcreorder,off \
+                 --set standby,241 \
+                 $disk
+    fi
 done
 

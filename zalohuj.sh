5d7f2bb8095544e6872871eaf7e5a8a7473f0e6a
Stanislav Graf <stanislav.graf@gmail.com>
2018-09-23 11:17:10 +0200

    Store hashes at permanent location

--- zalohuj.sh
+++ zalohuj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Deduplication DATA
-/usr/sbin/duperemove -hdr --hashfile=/var/tmp/duperemove_data.hash /media/DATA/
+/usr/sbin/duperemove -hdr --hashfile=/media/DATA/Zalohy/backup/duperemove_data.hash /media/DATA/
 
 # Balance DATA
 btrfs balance start -musage=0 -v /media/DATA/
@@ -12,18 +12,11 @@ btrfs balance start -dusage=20 -v /media/DATA/
 # Mount BACKUP RW
 mount -o remount,rw /media/BACKUP
 
-# Sync DATA with BACKUP
-#cp      --archive --no-clobber --verbose \
-#        --one-file-system /media/DATA/* /media/BACKUP/
-
 rsync   --archive --delete --verbose \
         --one-file-system /media/DATA/ /media/BACKUP/
 
 # Deduplication of BACKUP
-/usr/sbin/duperemove -hdr --hashfile=/var/tmp/duperemove_backup.hash /media/BACKUP/
-
-# Backup index files
-cp --force /var/tmp/duperemove* /media/DATA/Zalohy/backup/
+/usr/sbin/duperemove -hdr --hashfile=/media/DATA/Zalohy/backup/duperemove_backup.hash /media/BACKUP/
 
 # Balance BACKUP
 btrfs balance start -musage=0 -v /media/BACKUP/

00996ad8473fa1a924d0261d1570c7edd42323ab
Stanislav Graf <stanislav.graf@gmail.com>
2020-04-25 18:00:35 +0200

    Balance is no longer needed

--- zalohuj.sh
+++ zalohuj.sh
@@ -3,7 +3,6 @@
 # Deduplication DATA
 duperemove -hdr --hashfile=/root/duperemove_data.hash \
         --dedupe-options=noblock  /media/DATA/
-btrfs balance start -musage=20 -dusage=20 -v /media/DATA/
 
 # Mount BACKUP RW
 mount -o remount,rw /media/BACKUP
@@ -15,7 +14,6 @@ rsync   --archive --delete --verbose --executability \
 # Deduplication of BACKUP
 duperemove -hdr --hashfile=/root/duperemove_backup.hash \
         --dedupe-options=noblock  /media/BACKUP/
-btrfs balance start -musage=20 -dusage=20 -v /media/BACKUP/
 
 # Let the things calm down and move BACKUP back to RO
 sleep 2m

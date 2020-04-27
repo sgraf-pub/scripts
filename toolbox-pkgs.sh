09681f6eec91a8829fb7df4836febbc3aa679662
Stanislav Graf <stanislav.graf@gmail.com>
2020-04-27 11:55:57 +0200

    add toolbox setup

---
+++ toolbox-pkgs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-32.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-32.noarch.rpm
+sudo dnf install -y abcde ffmpeg lame mc mmv python3-eyed3 unrar vim vobcopy youtube-dl

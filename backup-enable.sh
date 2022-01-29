f5080b2a8dd36068919486df492b33eaeb51d7c4
Stanislav Graf <stanislav.graf@gmail.com>
2022-01-29 19:40:54 +0100

    update backup scripts to latest

--- backup-enable.sh
+++ backup-enable.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 read -s -p "Password: " DISK_PASS
-
-echo -n "$DISK_PASS" | cryptsetup luksOpen UUID=8c0048d5-e630-4e90-a969-1af6e34fab70 luks-8c0048d5-e630-4e90-a969-1af6e34fab70 -d -
-echo -n "$DISK_PASS" | cryptsetup luksOpen UUID=7247b2f5-7e6b-48bf-b4dd-aca59c596e1a luks-7247b2f5-7e6b-48bf-b4dd-aca59c596e1a -d -
-
-mkdir /media/BACKUP
-mount /dev/mapper/luks-7247b2f5-7e6b-48bf-b4dd-aca59c596e1a /media/BACKUP -o defaults,ro,compress-force=zstd:1
-
+echo -n "$DISK_PASS" | cryptsetup -v luksOpen UUID=03a53235-3f88-4d31-b97f-12857fe2f19f luks-03a53235-3f88-4d31-b97f-12857fe2f19f -d -
+echo -n "$DISK_PASS" | cryptsetup -v luksOpen UUID=8adb51f2-069f-4b62-9fb7-753bc60617d6 luks-8adb51f2-069f-4b62-9fb7-753bc60617d6 -d -
+echo -n "$DISK_PASS" | cryptsetup -v luksOpen UUID=4eff7329-c991-4739-b41d-6a50af5a2d95 luks-4eff7329-c991-4739-b41d-6a50af5a2d95 -d -
+echo -n "$DISK_PASS" | cryptsetup -v luksOpen UUID=0158d2c4-9b1e-4981-a2d1-728834d3e895 luks-0158d2c4-9b1e-4981-a2d1-728834d3e895 -d -
+mkdir -v /media/BACKUP
+mount /dev/mapper/luks-03a53235-3f88-4d31-b97f-12857fe2f19f /media/BACKUP -o defaults,compress-force=zstd:1 -v
+systemctl start beesd@b278045d-82ac-472f-a374-fb21afd1b906

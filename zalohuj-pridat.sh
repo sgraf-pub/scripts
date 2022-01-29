9212c7368cdb1796278b5fe25d20cc9fe89f6627
Stanislav Graf <stanislav.graf@gmail.com>
2022-01-29 19:43:21 +0100

    Update backup scripts to latest

--- zalohuj-pridat.sh
+++ zalohuj-pridat.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -ex
 
 # Mount BACKUP RW
-mount -o remount,rw /media/BACKUP
+systemctl stop beesd@b278045d-82ac-472f-a374-fb21afd1b906
+mount -v -o remount,rw /media/BACKUP
 
 # Backup
-rsync -aEHXSxv /media/DATA/ /media/BACKUP/
-rsync -aEHXSxv --delete --dry-run /media/DATA/ /media/BACKUP/
+rsync -aEHXSx --progress /media/DATA/data/ /media/BACKUP/data/
+rsync -aEHXSx --progress /media/DATA/zalohy/ /media/BACKUP/zalohy/
+rsync -aEHXSxv --delete --dry-run /media/DATA/data/ /media/BACKUP/data/
+rsync -aEHXSxv --delete --dry-run /media/DATA/zalohy/ /media/BACKUP/zalohy/
 
 # Let the things calm down and move BACKUP back to RO
-sleep 10s
-mount -o remount,ro /media/BACKUP
+sync
+mount -v -o remount,ro /media/BACKUP
+systemctl start beesd@b278045d-82ac-472f-a374-fb21afd1b906
--- zalohuj-smazat.sh
+++ zalohuj-smazat.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -ex
 
 # Mount BACKUP RW
-mount -o remount,rw /media/BACKUP
+systemctl stop beesd@b278045d-82ac-472f-a374-fb21afd1b906
+mount -v -o remount,rw /media/BACKUP
 
 # Backup
-rsync -aEHXSxv --delete /media/DATA/ /media/BACKUP/
+rsync -aEHXSxv --delete /media/DATA/data/ /media/BACKUP/data/
+rsync -aEHXSxv --delete /media/DATA/zalohy/ /media/BACKUP/zalohy/
 
 # Let the things calm down and move BACKUP back to RO
-sleep 10s
-mount -o remount,ro /media/BACKUP
+sync
+mount -v -o remount,ro /media/BACKUP
+systemctl start beesd@b278045d-82ac-472f-a374-fb21afd1b906

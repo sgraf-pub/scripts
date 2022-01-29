07da986350a0651721ac901fc7e36a3c4a76b82f
Stanislav Graf <stanislav.graf@gmail.com>
2022-01-29 19:40:00 +0100

    remove files no longer needed

--- data-restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-pkill rygel
-systemctl stop smb
-sudo -u medvidek /var/home/medvidek/.local/bin/wrap-dbus rygel -c /etc/rygel.conf &>/var/home/medvidek/.cache/rygel/rygel.log &
-systemctl start smb
-systemctl restart airsaned
